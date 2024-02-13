a1f4f07485bdc4e2298589d667261a034e72b023
Tarekrahaman931 <159681882+Tarekrahaman931@users.noreply.github.com>
2024-02-13 11:57:18 +1000

    Add prac 1 files

---
+++ prac_01/sales_bonus.py
@@ -0,0 +1,27 @@
+"""
+Program to calculate and display a user's bonus based on sales.
+If sales are under $1,000, the user gets a 10% bonus.
+If sales are $1,000 or over, the bonus is 15%.
+"""
+
+
+def calculate_bonus(sales):
+    if sales < 1000:
+        bonus = sales * 0.10
+    else:
+        bonus = sales * 0.15
+    return bonus
+
+
+def main():
+    sales = float(input("Enter sales: $"))
+
+    while sales >= 0:
+        bonus = calculate_bonus(sales)
+        print('Bonus: $', bonus)
+        sales = float(input("Enter sales: $"))
+
+    print("Program ended.")
+
+
+main()
